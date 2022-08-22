fd6db19152970fef010905d3b19b47027dc3f0a9
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-22 16:02:20 +0200

    Add modal dialog droplet
    ERM29382
    
    [4.x]
    
    Change-Id: Id17aaa4173d4d0597a4f0e33d1f8c88cf81196f7
    (cherry picked from commit 4dff663061276c39c2c5988a0f3d75ea781a93cb)
    (cherry picked from commit bb0e21f52e3fea10444f654f68045947afc7c146)

---
+++ resources/objects/ModalDialogDroplet.js
@@ -0,0 +1,45 @@
+// eslint-disable-next-line no-global-assign
+ext = ext || {};
+ext.proDistribution = ext.proDistribution || {};
+ext.proDistribution.objects = {};
+ext.proDistribution.objects.ModalDialogDroplet = function( cfg ) {
+	ext.proDistribution.objects.ModalDialogDroplet.parent.call( this, cfg );
+};
+OO.inheritClass( ext.proDistribution.objects.ModalDialogDroplet, ext.contentdroplets.object.TransclusionDroplet );
+ext.proDistribution.objects.ModalDialogDroplet.prototype.templateMatches = function( templateData ) {
+	if ( !templateData ) {
+		return false;
+	}
+	var target = templateData.target.wt;
+	return target.trim( '\n' ) === 'ModalDialog' && 'modal-dialog' === this.getKey();
+};
+ext.proDistribution.objects.ModalDialogDroplet.prototype.toDataElement = function( domElements, converter  ) {
+	return false;
+};
+ext.proDistribution.objects.ModalDialogDroplet.prototype.getFormItems = function() {
+	return [
+		{
+			name: 'btnLabel',
+			label: mw.message( 'droplets-modal-dialog-btn-label' ).plain(),
+			type: 'text'
+		},
+		{
+			name: 'title',
+			label: mw.message( 'droplets-modal-dialog-title-label' ).plain(),
+			type: 'text'
+		},
+		{
+			name: 'body',
+			label: mw.message( 'droplets-modal-dialog-body-label' ).plain(),
+			type: 'textarea',
+			row: 5
+		},
+		{
+			name: 'footer',
+			label: mw.message( 'droplets-modal-dialog-footer-label' ).plain(),
+			type: 'textarea',
+			row: 2
+		}
+	];
+};
+ext.contentdroplets.registry.register( 'modal-dialog', ext.proDistribution.objects.ModalDialogDroplet );
