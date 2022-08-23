435adbe02d680c720234c86758f8ce2d3d2a93ad
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-23 15:51:57 +0200

    Add decision droplet
    ERM29382
    
    [4.x]
    
    Change-Id: I4ac560c5bb4e0749697bdb11907c31ca24f6f4fb
    (cherry picked from commit 0fec235d5fe660ff165f6cf79d17084cea251d76)

---
+++ src/ContentDroplets/DecisionDroplet.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TemplateDroplet;
+use Message;
+
+class DecisionDroplet extends TemplateDroplet {
+
+	/**
+	 * Get target for the template
+	 * @return string
+	 */
+	protected function getTarget(): string {
+		return 'Decision';
+	}
+
+	/**
+	 * Template params
+	 * @return array
+	 */
+	protected function getParams(): array {
+		return [
+			'decision' => 'Use decision droplet'
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'bs-pro-distribution-droplet-decision-name' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return Message::newFromKey( 'bs-pro-distribution-droplet-decision-desc' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'droplet-decision';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.prodistributionconnector.droplet.decision' ];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCategories(): array {
+		return [ 'content' ];
+	}
+}
