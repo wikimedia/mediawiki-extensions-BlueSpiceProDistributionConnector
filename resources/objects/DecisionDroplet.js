8f5acc74d6272713749b99300e5be039f71d6175
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-23 15:51:57 +0200

    Add decision droplet
    ERM29382
    
    [4.x]
    
    Change-Id: I4ac560c5bb4e0749697bdb11907c31ca24f6f4fb
    (cherry picked from commit 0fec235d5fe660ff165f6cf79d17084cea251d76)

---
+++ resources/objects/DecisionDroplet.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-global-assign
+ext = ext || {};
+ext.proDistribution = ext.proDistribution || {};
+ext.proDistribution.objects = {};
+
+ext.proDistribution.objects.DecisionDroplet = function( cfg ) {
+	ext.proDistribution.objects.DecisionDroplet.parent.call( this, cfg );
+};
+
+OO.inheritClass( ext.proDistribution.objects.DecisionDroplet, ext.contentdroplets.object.TransclusionDroplet );
+
+ext.proDistribution.objects.DecisionDroplet.prototype.templateMatches = function( templateData ) {
+	if ( !templateData ) {
+		return false;
+	}
+	var target = templateData.target.wt;
+	return target.trim( '\n' ) === 'Decision' && 'decision' === this.getKey();
+};
+
+ext.proDistribution.objects.DecisionDroplet.prototype.toDataElement = function( domElements, converter  ) {
+	return false;
+};
+
+ext.proDistribution.objects.DecisionDroplet.prototype.getFormItems = function() {
+	return [
+		{
+			name: 'decision',
+			label: mw.message( 'droplets-decision-label' ).plain(),
+			type: 'textarea',
+			row: 2
+		},
+	];
+};
+
+ext.contentdroplets.registry.register( 'decision', ext.proDistribution.objects.DecisionDroplet );
