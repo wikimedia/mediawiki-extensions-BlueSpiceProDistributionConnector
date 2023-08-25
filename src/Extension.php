31ce9493ec331bb12693d94ea06ce67a2036cce5
miriam schlindwein <schlindwein@hallowelt.com>
2023-08-25 11:58:58 +0200

    Fix third party droplet registration
    ERM33478
    
    [4.3.x]
    
    Change-Id: I1d9f3b273be6b2e84875c8c9b38ddfda06f2f473
    (cherry picked from commit f9a2c54616cb2c922b755305be1bd495408ff0b1)
    (cherry picked from commit 97161d53ee82e4e057410001e872b1242118115c)

---
+++ src/Extension.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector;
+
+use ExtensionRegistry;
+
+class Extension extends \BlueSpice\Extension {
+
+	public static function onRegistration() {
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'PDFEmbed' ) ) {
+			$GLOBALS['wgContentDropletsDroplets']['pdf-embed'] = [
+				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\PDFEmbedDroplet"
+			];
+		}
+		if ( ExtensionRegistry::getInstance()->isLoaded( 'Math' ) ) {
+			$GLOBALS['wgContentDropletsDroplets']['math-form'] = [
+				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\MathFormDroplet"
+			];
+			$GLOBALS['wgContentDropletsDroplets']['chem-form'] = [
+				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\ChemFormDroplet"
+			];
+		}
+	}
+}
