e798418bb19676259b29996796f064274118a8eb
d.savuljesku <d.savuljesku@gmail.com>
2024-06-12 12:15:08 +0200

    Allow configuration of OAuth keys over ConfigManager
    [4.5.x]
    
    ERM37395
    
    Change-Id: I603a74242d1ff9b8f174c1f17d670bc19e8828e7

---
+++ src/ConfigDefinition/OAuth2PublicKey.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition;
+use ExtensionRegistry;
+use HTMLTextAreaField;
+
+class OAuth2PublicKey extends ConfigDefinition\StringSetting implements ConfigDefinition\IOverwriteGlobal {
+
+	/**
+	 *
+	 * @return HTMLTextAreaField
+	 */
+	public function getHtmlFormField() {
+		return new HTMLTextAreaField( $this->makeFormFieldParams() + [ 'rows' => 10 ] );
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_SYSTEM . '/BlueSpiceProDistributionConnector',
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceProDistributionConnector/' . static::FEATURE_SYSTEM ,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_PRO . '/BlueSpiceProDistributionConnector',
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return "bs-pro-distribution-config-outh-public-key";
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isHidden() {
+		return !ExtensionRegistry::getInstance()->isLoaded( 'OAuth' );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'wgOAuth2PublicKey';
+	}
+}
