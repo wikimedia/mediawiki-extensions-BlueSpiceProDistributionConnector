6379e3da7b4b1e8a78bc3367dc6c3d9c797ef97f
d.savuljesku <d.savuljesku@gmail.com>
2025-01-14 10:21:28 +0100

    Integrate LinkTitles to ConfigManager
    Depends-On: Id47862709b45f33325248bb6ee5de476096b712e
    
    [5.x]
    
    ERM39753
    
    Change-Id: Ia1ed1ce2033472a73d78a72bad5a936ce1a5e689
    (cherry picked from commit 9ccc33c24bd833e9b517973f9cffedda4e5591f8)

---
+++ src/ConfigDefinition/LinkTitlesSameNamespace.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition;
+use BlueSpice\ProDistributionConnector\EditionProvider;
+use Config;
+use IContextSource;
+use MediaWiki\MediaWikiServices;
+
+class LinkTitlesSameNamespace extends ConfigDefinition\BooleanSetting implements ConfigDefinition\IOverwriteGlobal {
+
+	/**
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+				static::MAIN_PATH_FEATURE . '/' . static::FEATURE_CONTENT_STRUCTURING . '/LinkTitles',
+				static::MAIN_PATH_EXTENSION . '/LinkTitles/' . static::FEATURE_CONTENT_STRUCTURING,
+				static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_PRO . '/LinkTitles',
+			];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return "bs-pro-distribution-config-link-titles-same-ns";
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'LinkTitlesSameNamespace';
+	}
+}
---
+++ src/ConfigDefinition/LinkTitlesTargetNamespaces.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\ConfigDefinition;
+
+use BlueSpice\ConfigDefinition;
+use Config;
+use IContextSource;
+use MediaWiki\Language\Language;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Title\NamespaceInfo;
+
+class LinkTitlesTargetNamespaces extends LinkTitlesSourceNamespaces {
+
+	/**
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return "bs-pro-distribution-config-link-titles-target-ns";
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHelpMessageKey() {
+		return "bs-pro-distribution-config-link-titles-target-ns-help";
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return 'LinkTitlesTargetNamespaces';
+	}
+}
