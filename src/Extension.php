7fbd0dcb8848739cd5672e3d65ce18b8227211af
Mschlindwein <schlindwein@hallowelt.com>
2025-02-25 13:00:55 +0100

    Fix droplet registration
    ERM40804
    
    Depends-On: I62c40ecd056f0df38e601ef7134d7e144f27f852
    
    [5.x]
    
    Change-Id: Ic9f57c85fdd1f56f63f81a9c2654e2a27439c672
    (cherry picked from commit a9f3a304346fa7c7e139624110b07b186667da39)

--- src/Extension.php
+++ src/Extension.php
@@ -2,23 +2,5 @@
 
 namespace BlueSpice\ProDistributionConnector;
 
-use MediaWiki\Registration\ExtensionRegistry;
-
 class Extension extends \BlueSpice\Extension {
-
-	public static function onRegistration() {
-		if ( ExtensionRegistry::getInstance()->isLoaded( 'PDFEmbed' ) ) {
-			$GLOBALS['wgContentDropletsDroplets']['pdf-embed'] = [
-				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\PDFEmbedDroplet"
-			];
-		}
-		if ( ExtensionRegistry::getInstance()->isLoaded( 'Math' ) ) {
-			$GLOBALS['wgContentDropletsDroplets']['math-form'] = [
-				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\MathFormDroplet"
-			];
-			$GLOBALS['wgContentDropletsDroplets']['chem-form'] = [
-				"class" => "\\BlueSpice\\ProDistributionConnector\\ContentDroplets\\ChemFormDroplet"
-			];
-		}
-	}
 }
