5d0ec5c020571f94760fc8f8488274f1672d4a03
d.savuljesku <d.savuljesku@gmail.com>
2025-03-19 13:49:10 +0100

    Add LoginNotify events to NotifyMe
    [5.x]
    
    ERM41439
    
    Change-Id: If620d8aa94643bed7ea234308e4607dadfd00546
    (cherry picked from commit 4c7cc5fcbbe49892604205904b16180996f34c5a)

---
+++ src/Event/LoginNotifyFailKnown.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\Event;
+
+use Message;
+use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
+
+class LoginNotifyFailKnown extends LoginNotifySuccess {
+
+	/**
+	 * @var int
+	 */
+	private $count;
+
+	/**
+	 * @param array $info
+	 */
+	public function __construct( array $info ) {
+		parent::__construct( $info );
+		$this->count = $info['count'] ?? 1;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return 'login-notify-fail-known';
+	}
+
+	/**
+	 * @param IChannel $forChannel
+	 * @return Message
+	 */
+	public function getMessage( IChannel $forChannel ): Message {
+		return Message::newFromKey( 'notification-known-header-login-fail', $this->count );
+	}
+
+	/**
+	 * @param IChannel $forChannel
+	 * @return array|\MWStake\MediaWiki\Component\Events\EventLink[]
+	 */
+	public function getLinks( IChannel $forChannel ): array {
+		return [];
+	}
+}
