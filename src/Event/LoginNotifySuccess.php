4c7cc5fcbbe49892604205904b16180996f34c5a
d.savuljesku <d.savuljesku@gmail.com>
2025-03-19 13:49:10 +0100

    Add LoginNotify events to NotifyMe
    [5.x]
    
    ERM41439
    
    Change-Id: If620d8aa94643bed7ea234308e4607dadfd00546

---
+++ src/Event/LoginNotifySuccess.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BlueSpice\ProDistributionConnector\Event;
+
+use MediaWiki\User\UserIdentity;
+use Message;
+use MWStake\MediaWiki\Component\Events\BotAgent;
+use MWStake\MediaWiki\Component\Events\Delivery\IChannel;
+use MWStake\MediaWiki\Component\Events\NotificationEvent;
+
+class LoginNotifySuccess extends NotificationEvent {
+
+	/**
+	 * @var UserIdentity
+	 */
+	private $target;
+
+	/**
+	 * @param array $info
+	 */
+	public function __construct( array $info ) {
+		parent::__construct( new BotAgent() );
+		$this->target = $info['agent'];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getKey(): string {
+		return 'login-notify-success';
+	}
+
+	/**
+	 * @return array|UserIdentity[]|null
+	 */
+	public function getPresetSubscribers(): ?array {
+		return [ $this->target ];
+	}
+
+	/**
+	 * @param IChannel $forChannel
+	 * @return Message
+	 */
+	public function getMessage( IChannel $forChannel ): Message {
+		return Message::newFromKey( 'notification-header-login-success', $this->getAgent() );
+	}
+
+	/**
+	 * @param IChannel $forChannel
+	 * @return array|\MWStake\MediaWiki\Component\Events\EventLink[]
+	 */
+	public function getLinks( IChannel $forChannel ): array {
+		return [];
+	}
+}
