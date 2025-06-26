c4a633776647e588ea71c301c38609beb205b79d
d.savuljesku <d.savuljesku@gmail.com>
2025-06-26 15:45:19 +0200

    Use env var to check for BS edition
    [5.0.x]
    
    ERM42779
    
    Change-Id: Idcb5a650704a5319254e43ff0dbba045054a52f6
    (cherry picked from commit efe94a049b50810310ffaf0917adc9bc4c89f47f)

--- src/EditionProvider.php
+++ src/EditionProvider.php
@@ -8,6 +8,10 @@ class EditionProvider {
 	 * @return string|null if no edition info can be found
 	 */
 	public function getEdition(): ?string {
+		$edition = getenv( 'EDITION' );
+		if ( $edition ) {
+			return $edition;
+		}
 		$file = $GLOBALS['IP'] . '/BLUESPICE-EDITION';
 		if ( !file_exists( $file ) ) {
 			return null;
